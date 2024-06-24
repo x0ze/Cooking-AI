cae8aa286ad9f4f3ba6ae8a1aa619094bf295a2b
evemoser <130033934+evemoser@users.noreply.github.com>
2024-06-24 10:33:00 +0200

    Update recipe-complete.tsx

--- lethimcook-app/components/recipe-complete.tsx
+++ lethimcook-app/components/recipe-complete.tsx
@@ -96,18 +96,6 @@ export function RecipeComplete() {
                 <CardTitle className="text-sm font-medium">
                   Calorie
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                </svg>
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">529 kcal</div>
@@ -121,20 +109,6 @@ export function RecipeComplete() {
                 <CardTitle className="text-sm font-medium">
                   Glucide
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">20 g</div>
@@ -146,19 +120,6 @@ export function RecipeComplete() {
             <Card className="m-2">
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">Protéine</CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <rect width="20" height="14" x="2" y="5" rx="2" />
-                  <path d="M2 10h20" />
-                </svg>
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">15 g</div>
