7bebe8c4120c955addf826fb33dc83a4023f2691
evemoser <130033934+evemoser@users.noreply.github.com>
2024-06-25 14:34:27 +0200

    Update skeleton.tsx

--- lethimcook-app/components/ui/skeleton.tsx
+++ lethimcook-app/components/ui/skeleton.tsx
@@ -1,35 +1,15 @@
-import React from 'react';
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-// Image path for images in the `public` folder should be relative to the public root
-const imagePath = "/generate.png";
-
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Skeleton: React.FC<SkeletonProps> = ({ className, ...props }) => {
-  // Styles pour l'image avec bordures arrondies et effet de survol
-  const imageStyles: React.CSSProperties = {
-    borderRadius: '20px', // Bords arrondis
-    transition: 'transform 0,3s ease', // Transition pour l'effet de survol
-    cursor: 'pointer' // Curseur pointer au survol
-  };
-
-  // Styles pour l'image au survol
-  const hoverStyles: React.CSSProperties = {
-    transform: 'scale(1.1)' // Agrandissement au survol
-  };
-
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn("skeleton-container", className)} {...props}>
-      <img
-        src={imagePath}
-        alt="Description de l'image"
-        style={imageStyles}
-        onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.010)')} // Gérer le survol avec JS
-        onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}  // Retour à la taille normale
-      />
-    </div>
-  );
-};
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  )
+}
 
-export { Skeleton };
+export { Skeleton }
